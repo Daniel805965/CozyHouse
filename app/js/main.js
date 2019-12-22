862d8ff0b527f2154814366e3625777aace51334
Daniel805965 <danieldiekmannjr@gmail.com>
2019-12-22 19:02:41 +0200

    add homework

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
-$(function(){
+var swiper = new Swiper('.js-our-friends', {
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.js-our-friends-next',
+      prevEl: '.js-our-friends-prev',
+    },
+  });
 
-
-
-  
-});
+  var swiper = new Swiper('.Pets-swiper', {
+    slidesPerView: 8,
+    slidesPerColumn: 2,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
